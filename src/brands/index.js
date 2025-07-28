00aed1f04b37cedd0881c80059f166195d7ffc8c
Ana Sadeghian <asadeghi@hiiakasessence.org>
2025-07-28 14:00:54 -0400

    Fix: Switched default brand to universal in brand config

--- src/brands/index.js
+++ src/brands/index.js
@@ -1,13 +1,13 @@
 // brands/index.js
 
 import biocidin from "./biocidin.js";
+import universal from "./universal.js";
 
 const brandMap = {
   biocidin,
-  // future brands go here, like:
-  // "gutgenius": import("./gutgenius.js")
+  universal,
 };
 
-export default function getBrandConfig(name = "biocidin") {
-  return brandMap[name] || brandMap["biocidin"];
+export default function getBrandConfig(name = "universal") {
+  return brandMap[name] || brandMap["universal"];
 }
